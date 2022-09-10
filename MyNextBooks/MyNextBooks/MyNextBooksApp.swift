1e6dcf0790a82d91ec5aa861b1368bb69013e0c3
Erich Flock <erich.flock@check24.de>
2022-09-10 19:24:07 +0200

    Create project

---
+++ MyNextBooks/MyNextBooks/MyNextBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyNextBooksApp.swift
+//  MyNextBooks
+//
+//  Created by Erich Flock on 10.09.22.
+//
+
+import SwiftUI
+
+@main
+struct MyNextBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
