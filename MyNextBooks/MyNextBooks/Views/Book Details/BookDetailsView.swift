7b7e2bc860ad5f0dddc62c611a7ef9aa88bd2824
Erich Flock <erich.flock@check24.de>
2022-10-06 18:12:49 +0200

    Create initial structure of book details view

---
+++ MyNextBooks/MyNextBooks/Views/Book Details/BookDetailsView.swift
@@ -0,0 +1,40 @@
+//
+//  BookDetailsView.swift
+//  MyNextBooks
+//
+//  Created by Erich Flock on 06.10.22.
+//
+
+import SwiftUI
+
+struct BookDetailsView: View {
+    
+    let book: Book
+    
+    var body: some View {
+        VStack(alignment: .center, spacing: 8) {
+            AsyncImage(url: URL(string: book.getSecureImageUrl() ?? "")) { image in
+                image
+                    .resizable()
+            } placeholder: {
+                Image(systemName: "book")
+                    .resizable()
+            }
+            .frame(width: 128, height: 192)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            
+            Text(book.title)
+                .font(.title2)
+            Text(book.authors)
+                .font(.title3)
+                .foregroundColor(.secondary)
+            Spacer()
+        }
+    }
+}
+
+struct BookDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        BookDetailsView(book: .init(title: "", authors: ""))
+    }
+}
