c463efc810c18b7b4ead3157830f87f710e3946c
Erich Flock <erich.flock@check24.de>
2022-10-07 17:51:13 +0200

    Create initial structure of reading list view

---
+++ MyNextBooks/MyNextBooks/Views/Reading List/ReadingListView.swift
@@ -0,0 +1,45 @@
+//
+//  ReadingListView.swift
+//  MyNextBooks
+//
+//  Created by Erich Flock on 07.10.22.
+//
+
+import SwiftUI
+
+struct ReadingListView: View {
+    
+    @ObservedObject var readingManager = ReadingListManager.shared
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(readingManager.readingList) { book in
+                    NavigationLink(destination: BookDetailsView(book: book)) {
+                        BookCell(book: book)
+                    }
+                }
+            }
+            .navigationTitle("Reading List")
+            .overlay {
+                if readingManager.readingList.isEmpty {
+                    VStack(alignment: .center, spacing: 10) {
+                        Image("noSearchResults")
+                            .resizable()
+                            .frame(width: 80, height: 80, alignment: .center)
+                        Text("No books. \nPlease add books to your list.")
+                            .foregroundColor(.secondary)
+                            .multilineTextAlignment(.center)
+                    }
+                    .offset(y: -50)
+                }
+            }
+        }
+    }
+}
+
+struct ReadingListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ReadingListView()
+    }
+}
