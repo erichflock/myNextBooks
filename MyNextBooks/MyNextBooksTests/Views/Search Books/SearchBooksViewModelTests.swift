7d8cb21bcf1da0d7bb40fddbba06dc4f476ad035
Erich Flock <erich.flock@check24.de>
2023-02-24 16:28:22 +0100

    Test searchBooks func

---
+++ MyNextBooks/MyNextBooksTests/Views/Search Books/SearchBooksViewModelTests.swift
@@ -0,0 +1,47 @@
+//
+//  SearchBooksViewModelTests.swift
+//  MyNextBooksTests
+//
+//  Created by Erich Flock on 24.02.23.
+//
+
+import XCTest
+@testable import MyNextBooks
+
+final class SearchBooksViewModelTests: XCTestCase {
+    
+    private var sut: SearchBooksViewModel!
+    
+    override func setUpWithError() throws {
+        sut = SearchBooksViewModel()
+    }
+
+    override func tearDownWithError() throws {
+        sut = nil
+    }
+    
+    func test_searchBooks_whenBooksFound_shouldUpdateBooks() async {
+        let booksManager = BooksManagerFake()
+        booksManager.books = [.fixture(id: "123"),
+                              .fixture(id: "321")]
+        sut.booksManager = booksManager
+        XCTAssertTrue(sut.books.isEmpty, "precondition")
+        
+        await sut.searchBooks()
+        
+        XCTAssertEqual(sut.books.count, 2)
+        XCTAssertEqual(sut.books[0].id, "123")
+        XCTAssertEqual(sut.books[1].id, "321")
+    }
+    
+}
+
+private class BooksManagerFake: BooksManager {
+    
+    var books: [Book] = []
+    
+    override func getBooks(with terms: String) async -> [Book] {
+        return books
+    }
+    
+}
