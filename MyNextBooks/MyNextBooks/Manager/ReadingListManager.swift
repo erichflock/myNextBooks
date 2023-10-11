e44f3ed2c8d826dc85f19274811312720c9e3c68
Erich Flock <erich.flock@check24.de>
2023-10-11 13:50:27 +0200

    Integrate with iCloud
    - Add and delete functions implemented

--- MyNextBooks/MyNextBooks/Manager/ReadingListManager.swift
+++ MyNextBooks/MyNextBooks/Manager/ReadingListManager.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import CloudKit
 
 protocol ReadingListManagerProtocol {
     var readingList: [Book] { get set }
@@ -32,12 +33,14 @@ class ReadingListManager: ObservableObject, ReadingListManagerProtocol {
     func add(book: Book) {
         guard !readingList.contains(book) else { return }
         readingList.append(book)
+        storeInCloud(book: book)
     }
     
     func remove(book: Book) {
         if let index = readingList.firstIndex(where: { $0 == book }) {
             readingList.remove(at: index)
         }
+        removeFromCloud(book: book)
     }
     
     //Used on list delete action
@@ -62,6 +65,33 @@ class ReadingListManager: ObservableObject, ReadingListManagerProtocol {
             UserDefaults.standard.set(data, forKey: readingListKey)
         } catch let error {
             print("Error encoding: \(error)")
+        }
+    }
+    
+    private func storeInCloud(book: Book) {
+        let bookRecord = CKRecord(recordType: "Book", recordID: .init(recordName: book.id))
+        bookRecord["title"] = book.title as CKRecordValue
+        bookRecord["authors"] = book.authors as CKRecordValue
+        bookRecord["imageUrl"] = book.imageUrl as? CKRecordValue
+        bookRecord["publishedDate"] = book.publishedDate as? CKRecordValue
+        bookRecord["description"] = book.description as? CKRecordValue
+        
+        CKContainer(identifier: "iCloud.MyNextBooks").privateCloudDatabase.save(bookRecord) { record, error in
+            if let error {
+                print("book not saved in the cloud. Error: \(error.localizedDescription)")
+            } else {
+                print("book saved in the cloud")
+            }
+        }
+    }
+    
+    private func removeFromCloud(book: Book) {
+        CKContainer(identifier: "iCloud.MyNextBooks").privateCloudDatabase.delete(withRecordID: .init(recordName: book.id)) { recordID, error  in
+            if let error {
+                print("could not deleted record: \(String(describing: recordID)). Error: \(error.localizedDescription)")
+            } else {
+                print("record: \(String(describing: recordID)) deleted")
+            }
         }
     }
     
