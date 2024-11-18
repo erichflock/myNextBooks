689c2467e48ea0c8d77e746a2ab48d39e3ced473
Erich Flock <erich.flock@check24.de>
2024-11-18 16:12:56 +0100

    Make additional information view reusable

--- MyNextBooks/MyNextBooks/Views/Book Details/BookDetailsView.swift
+++ MyNextBooks/MyNextBooks/Views/Book Details/BookDetailsView.swift
@@ -48,6 +48,7 @@ struct BookDetailsView: View {
                     }
                     .accessibilityIdentifier("bookDetails_heartButton")
                 }
+
                 if let description = book.description {
                     CollapsibleTextView(
                         text: description,
@@ -59,69 +60,58 @@ struct BookDetailsView: View {
                     }
                 }
                 
-                HStack(alignment: .firstTextBaseline) {
-                        if let language = book.language {
-                            VStack(spacing: 0) {
-                                Image(systemName: "globe")
-                                    .font(.title2)
-                                Spacer()
-                                Text(language)
-                                    .font(.caption)
-                                    .foregroundColor(.secondary)
-                                Spacer()
-                            }
-                        }
-                        
-                        if let pageCount = book.pageCount {
-                            VStack(spacing: 0) {
-                                Image(systemName: "ruler")
-                                    .font(.title2)
-                                Spacer()
-                                Text(pageCount == 1 ? "\(pageCount) page" : "\(pageCount) pages")
-                                    .font(.caption)
-                                    .foregroundColor(.secondary)
-                                Spacer()
-                            }
-                        }
+                HStack(alignment: .firstTextBaseline, spacing: 0) {
+                    if let language = book.language {
+                        additionalInformationView(
+                            systemImageName: "globe",
+                            text: language
+                        )
+                    }
+                    if let pageCount = book.pageCount?.description {
+                        additionalInformationView(
+                            systemImageName: "ruler",
+                            text: pageCount
+                        )
+                    }
                     if let publishedDate = book.publishedDate {
-                        VStack(spacing: 0) {
-                            Image(systemName: "calendar")
-                                .font(.title2)
-                            Spacer()
-                            Text(publishedDate)
-                                .font(.caption)
-                                .foregroundColor(.secondary)
-                            Spacer()
-                        }
+                        additionalInformationView(
+                            systemImageName: "calendar",
+                            text: publishedDate
+                        )
                     }
                     if let publisher = book.publisher {
-                        VStack(spacing: 0) {
-                            Image(systemName: "building.2")
-                                .font(.title2)
-                            Spacer()
-                            Text(publisher)
-                                .font(.caption)
-                                .foregroundColor(.secondary)
-                                .lineLimit(2)
-                            Spacer()
-                        }
+                        additionalInformationView(
+                            systemImageName: "building.2",
+                            text: publisher
+                        )
                     }
                     if let price = book.price {
-                        VStack(spacing: 0) {
-                            Image(systemName: "dollarsign.circle")
-                                .font(.title2)
-                            Spacer()
-                            Text(price)
-                                .font(.caption)
-                                .foregroundColor(.secondary)
-                            Spacer()
-                        }
+                        additionalInformationView(
+                            systemImageName: "dollarsign",
+                            text: price
+                        )
                     }
                 }
+                .padding(.top, 24)
                 Spacer()
             }
             .padding([.horizontal, .bottom])
         }
+    }
+    
+    private func additionalInformationView(systemImageName: String, text: String) -> some View {
+        VStack(spacing: 0) {
+            Image(systemName: systemImageName)
+                .font(.title2)
+            Spacer()
+            Text(text)
+                .font(.caption)
+                .foregroundColor(.secondary)
+                .lineLimit(2)
+                .multilineTextAlignment(.center)
+            Spacer()
+        }
+        .frame(width: 66)
     }
 }
 
@@ -136,7 +126,7 @@ struct BookDetailsView_Previews: PreviewProvider {
                 description: "Der Millionenseller jetzt aktualisiert und mit neuem Nachwort. Vor 100.000 Jahren lebte Homo Sapiens als unbedeutende Spezies in einem abgelegenen Winkel des afrikanischen Kontinents. Heute ist der Mensch Herr und Schrecken des Planeten. Wie konnte es dazu kommen? In seiner fulminanten Reise von den Menschenaffen bis zum Cyborg entwirft Yuval Noah Harari mit seinem international gefeierten Bestseller »Sapiens - Eine kurze Geschichte der Menschheit« das große Panorama unserer eigenen Geschichte – und stellt die Frage, wohin wir von hier aus gehen wollen. »Sapiens« ist einer der größten Sachbucherfolge aller Zeiten und hat allein in Deutschland, bisher unter dem Titel »Eine kurze Geschichte der Menschheit«, über 2 Millionen Exemplare verkauft. Dieser immense Erfolg ist kein Zufall: Das Buch hat von Grund auf verändert, welche Verantwortung wir als Menschen gegenüber unseren Mitgeschöpfen und dem Planeten empfinden – und wie wir handeln. Yuval Noah Harari schreibt präzise, klug – und vor allem so, dass man gar nicht aufhören will zu lesen. Dieses Buch lässt Hirne wachsen.« ZEIT WISSEN. Das Buch erschien erstmals 2013 unter dem Titel »Eine kurze Geschichte der Menschheit« bei DVA. Diese neue Ausgabe ist aktualisiert und um ein neues Nachwort ergänzt. Mit zahlreichen Abbildungen",
                 pageCount: 325,
                 language: "DE",
-                publisher: "Campus Verlag dsdfsdfsdsdfsd",
+                publisher: "Campus Verlag",
                 price: "19,99 €"
             )
         )
